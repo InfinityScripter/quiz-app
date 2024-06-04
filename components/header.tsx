f007a21988e3c0d4387c5977e8524f5723b51107
InfinityScripter <talalaev.misha@gmail.com>
2024-06-04 15:25:58 +0300

    style : add icons

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import Link from "next/link";
+import {FaHome, FaMailBulk, FaQuestion, FaStar} from "react-icons/fa";
 
 const Header = () => {
     return (
         <header className="bg-purple-600 text-white p-4 flex justify-between items-center">
-            <h1 className="text-xl font-bold">Quiz App</h1>
+            <div
+            className="flex items-center"
+            >
+            <FaStar
+                className="inline-block mr-2.5"
+            />
+            <h1 className="text-xl font-bold">
+
+                Quiz App</h1></div>
             <nav>
-                <Link href="/" className="mx-2 underline">Главная</Link>
-                <Link href="/about" className="mx-2 underline">Обо мне</Link>
-                <Link href="/contact" className="mx-2 underline">Контакты</Link>
+                <Link href="/" className="mx-2 underline">
+
+                    <FaHome className="inline-block mr-2.5" />
+                    Главная</Link>
+                <Link href="/about" className="mx-2 underline">
+                    <FaQuestion className="inline-block mr-2.5" />
+                    О квизе</Link>
+                <Link href="/contact" className="mx-2 underline">
+                    <FaMailBulk className="inline-block mr-2.5" />
+                    Контакты</Link>
             </nav>
         </header>
     );
