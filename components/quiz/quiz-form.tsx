6c1e1c2316e6f210a6ac11cac015036986612907
InfinityScripter <talalaev.misha@gmail.com>
2024-06-05 09:28:26 +0300

    feat : add Timer , count question style : result page

--- components/quiz/quiz-form.tsx
+++ components/quiz/quiz-form.tsx
@@ -5,7 +5,6 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
-
 import { Button } from "@/components/ui/button";
 import {
     Form,
@@ -18,10 +17,12 @@ import {
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { FormSuccess } from "@/components/form-success";
 import { FormError } from "@/components/form-error";
+import { Progress } from "@/components/ui/progress";
+import { Timer } from "lucide-react";
 import { questions } from "@/lib/questions";
 
 const FormSchema = z.object({
-    answer: z.string().nonempty("Выберите вариант ответа."),
+answer: z.string().nonempty("Выберите ответ"),
 });
 
 export function QuizForm() {
@@ -30,9 +31,10 @@ export function QuizForm() {
     const [isAnswered, setIsAnswered] = useState(false);
     const [correctAnswers, setCorrectAnswers] = useState(0);
     const [isClient, setIsClient] = useState(false);
-    console.log(correctAnswers);
-    console.log(isCorrect);
-    console.log(questions.length)
+    const [timeLeft, setTimeLeft] = useState(300);
+    const [timerStarted, setTimerStarted] = useState(false);
+    const router = useRouter();
+
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
     });
@@ -41,7 +43,24 @@ export function QuizForm() {
         setIsClient(true);
     }, []);
 
-    const router = useRouter();
+    useEffect(() => {
+        if (!timerStarted) return;
+
+        if (timeLeft === 0) {
+            router.push(`/result?correctAnswers=${correctAnswers}&totalQuestions=${questions.length}`);
+            return;
+        }
+
+        const timer = setInterval(() => {
+            setTimeLeft(timeLeft - 1);
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, [timeLeft, timerStarted]);
+
+    const handleStart = () => {
+        setTimerStarted(true);
+    };
 
     const handleNextQuestion = (data: z.infer<typeof FormSchema>) => {
         const currentQuestion = questions[currentQuestionIndex];
@@ -63,16 +82,32 @@ export function QuizForm() {
                 setIsAnswered(false);
                 form.reset();
             }
-        }, 2000);
+        }, 1000);
     };
 
     if (!isClient) {
         return null;
     }
 
+    if (!timerStarted) {
+        return (
+            <div className="flex flex-col items-center justify-center  bg-gray-100 p-4 rounded-xl shadow-lg">
+                <Button variant="purple" onClick={handleStart}>Начать тест</Button>
+            </div>
+        );
+    }
+
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(handleNextQuestion)} className="w-2/3 space-y-6">
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center">
+                        <Timer size={32} className="mr-2 text-purple-800" />
+                        <span>{`${Math.floor(timeLeft / 60)}:${(timeLeft % 60).toString().padStart(2, '0')}`}</span>
+                    </div>
+                    <h3 className="text-xl font-bold">Вопрос {currentQuestionIndex + 1} из {questions.length}</h3>
+                </div>
+                <Progress value={(currentQuestionIndex / questions.length) * 100} className="my-4" />
                 <h2>{questions[currentQuestionIndex].question}</h2>
                 <FormField
                     control={form.control}
@@ -103,8 +138,8 @@ export function QuizForm() {
                 />
                 {isCorrect === true && <FormSuccess message="Верно!" />}
                 {isCorrect === false && <FormError message="Неверно!" />}
-                <Button type="submit" disabled={isAnswered}>
-                    Следующий вопрос
+                <Button variant="purple" type="submit" disabled={isAnswered}>
+                  Ответить
                 </Button>
             </form>
         </Form>
--- components/quiz/result-form.tsx
+++ components/quiz/result-form.tsx
@@ -3,26 +3,38 @@
 import { useSearchParams, useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
 
-const Result = () => {
+const ResultForm = () => {
     const searchParams = useSearchParams();
     const correctAnswers = parseInt(searchParams.get('correctAnswers') || '0', 10);
     const totalQuestions = parseInt(searchParams.get('totalQuestions') || '0', 10);
     const router = useRouter();
 
-    const completionPercentage = ((correctAnswers / totalQuestions) * 100).toFixed(2);
+    const completionPercentage = ((correctAnswers / totalQuestions) * 100).toFixed(0);
 
     if (isNaN(correctAnswers) || isNaN(totalQuestions)) {
         return <div>Нет данных для отображения результатов.</div>;
     }
 
     return (
-        <div>
-            <h3>Результаты</h3>
-            <p>Вы ответили правильно на {correctAnswers} из {totalQuestions} вопросов.</p>
-            <p>Процент правильных ответов: {completionPercentage}%</p>
-            <Button variant="purple" onClick={() => router.push('/')}>Пройти тест еще раз</Button>
+        <div className="flex flex-col items-center justify-center  bg-gray-50 p-4">
+            <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+                <h3 className="text-2xl font-bold mb-4">Результаты</h3>
+                <p className="text-lg">Вы ответили правильно на <span
+                className="font-bold text-purple-600"
+                >{correctAnswers}</span> из <span
+                className="font-bold text-purple-600"
+                >{totalQuestions}</span> вопросов.</p>
+                <p className="text-lg mb-4">Процент правильных ответов:
+                   <span
+                   className="font-bold text-purple-600"
+                   >{completionPercentage}%</span>
+                </p>
+                <div className="flex justify-center">
+                    <Button variant="purple" onClick={() => router.push('/')}>Пройти тест еще раз</Button>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default Result;
+export default ResultForm;
